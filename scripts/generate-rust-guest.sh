197a7ada4a6c0cfc788738bd56beee94787b10bf
xlem <xlem@akkoro.io>
2023-01-30 17:50:12 -0330

    fix script name in workflow

--- scripts/generate-rust-guest.sh
+++ scripts/generate-rust-guest.sh
@@ -2,10 +2,11 @@
 
 OUT_DIR=./out/wasi-secrets
 SRC_DIR=$OUT_DIR/src
+VERSION="0.0.0"
 
 wit-bindgen guest rust --out-dir $SRC_DIR --rustfmt wit/wasi-secrets.wit
 mv $SRC_DIR/wasi-secrets.rs $SRC_DIR/lib.rs
 echo "[package]
 name = \"wasi-secrets\"
-version = \"0.0.0\"
+version = \"$VERSION\"
 " >> $OUT_DIR/Cargo.toml
